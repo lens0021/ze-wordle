4cf386a12ae8bda5f2deb8079dd38c153b2736a3
lens0021 <lorentz0021@gmail.com>
2022-02-13 11:24:01 +0900

    More l10n

--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -6,8 +6,7 @@ export const ABOUT_GAME_MESSAGE = '이 게임에 대하여'
 // Unused in this variant
 export const NOT_ENOUGH_LETTERS_MESSAGE = 'Not enough letters'
 export const WORD_NOT_FOUND_MESSAGE = '포켓몬 이름이 아닙니다'
-export const CORRECT_WORD_MESSAGE = (solution: string) =>
-  `The word was ${solution}`
+export const CORRECT_WORD_MESSAGE = (solution: string) => `정답은 ${solution}`
 export const ENTER_TEXT = 'Enter'
 export const DELETE_TEXT = 'Delete'
 export const STATISTICS_TITLE = '통계'
